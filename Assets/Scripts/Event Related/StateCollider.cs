d60a101f7a9c01efd43f51e271fcb2f1fc6c966f
ficticious <magustinmeli@gmail.com>
2024-11-04 22:56:45 -0300

    sad

--- Assets/Scripts/Event Related/StateCollider.cs
+++ Assets/Scripts/Event Related/StateCollider.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
 
 public class StateCollider : MonoBehaviour
 {
